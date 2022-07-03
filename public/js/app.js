56b3a93be0f968ac2b4e82e5379cf71d8a1ff1e5
Fujimoto <gegvp0330@gmail.com>
2022-07-03 10:08:45 +0900

    js修正

--- public/js/app.js
+++ public/js/app.js
@@ -1223,12 +1223,3 @@ document.getElementById('delete-button').onclick = function() {
     return false;
   }
 }
-// function deleteAlert(e) {
-
-//   if(!window.confirm('本当に削除しますか？')) {
-//       window.alert('キャンセルされました');
-//       return false;
-//   }
-  
-//   document.deleteform.submit();
-// }
